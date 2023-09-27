108badccb04f1c11d65847e3e271a8f7717db78f
Pablo Gallina <pablogallinag@gmail.com>
2023-09-26 22:32:32 -0600

    add motion animation

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,13 +3,40 @@ extends KinematicBody2D
 export var speed : float = 100
 export var min_distance: float = 40
 
+# animation variables
+var is_running : bool = false
+var facing_left : bool = false
+
 func _process(delta):
 	motion_ctrl()
+	set_player_animation()
 
 func motion_ctrl() -> void:
 	var input_vector = get_input_vector_keyboard()
 	var velocity = input_vector * speed
+	
+	if velocity != Vector2.ZERO:
+		is_running = true
+	else:
+		is_running = false
+	
+	if velocity.x < 0:
+		facing_left = true
+	elif velocity.x > 0:
+		facing_left = false
+	
 	move_and_slide(velocity)
+
+func set_player_animation() -> void:
+	if is_running:
+		$spr_player2.play("run")
+	else:
+		$spr_player2.play("idle")
+	
+	if facing_left:
+		$spr_player2.flip_h = true
+	else:
+		$spr_player2.flip_h = false
 
 func get_input_vector_keyboard() -> Vector2:
 	var input_vector = Vector2.ZERO
